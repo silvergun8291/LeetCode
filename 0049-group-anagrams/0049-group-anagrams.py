670a10fbdb3f58fcbdf3487ae69f2632b68549a1
silvergun8291 <81484874+silvergun8291@users.noreply.github.com>
2023-02-17 22:42:47 +0900

    Update 0049-group-anagrams.py

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,9 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
         anagrams = collections.defaultdict(list)
-        print(anagrams)
 
-        for word in strs:
+        # 애너그램 -> 알파벳 순으로 정렬하면 같은 값이 나옴
+        for word in strs:   # 알파벳 순으로 정렬한 값을 키로 딕셔너리를 만듦
             anagrams[''.join(sorted(word))].append(word)
 
         return list(anagrams.values())
