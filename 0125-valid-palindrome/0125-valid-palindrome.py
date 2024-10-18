f7d3d14e6ba3afb1ed04a263fa7ca5691f48ddac
silvergun8291 <81484874+silvergun8291@users.noreply.github.com>
2024-10-18 17:27:35 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,4 +1,5 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = "".join(list(filter(lambda x: x.isalnum(), s))).lower()
+        s = [ch for ch in s.lower() if 'a' <= ch <= 'z' or 'A' <= ch <= 'Z']
         return s == s[::-1]
+        
